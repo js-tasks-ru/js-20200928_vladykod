3b68f4e065ede9bd7c5b5025ab4fab89fd15b170
vladykod <Vladyko@htmacademy.ru>
2020-10-05 23:01:14 +0300

    переделано 1е задание, все упрощено

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -7,42 +7,18 @@
 
 export function sortStrings(arr, param = 'asc') {
 
-  const enLang = ['65', '122'];
-  const ruLang = ['1025', '1105'];
-  let locale = '';
-  const sorted = [];
+  const sorted = [...arr];
+  const collator = new Intl.Collator(['ru', 'en'], {caseFirst: 'upper'});
 
-
-  for (let i = 0; i < arr.length; i++) {
-    sorted.push(arr[i]);
-  }
-
-  if (sorted[0].charCodeAt(0) > enLang[0] && sorted[0].charCodeAt(0) < enLang[1]) {
-    locale = 'en';
-  } else if (sorted[0].charCodeAt(0) > ruLang[0] && sorted[0].charCodeAt(0) < ruLang[1]) {
-    locale = 'ru';
-  } else {
-    alert('Допускаются только русский и английский языки');
-    return sorted;
-  }
-
-  for (let j = arr.length - 1 ; j > 0; j--) {
-    for (let i = 0; i < j; i++) {
-      if (param === 'asc') {
-        if (new Intl.Collator(locale, {caseFirst: 'upper'}).compare(sorted[i], sorted[i + 1]) > 0) {
-          let buf = sorted[i];
-          sorted[i] = sorted[i + 1];
-          sorted[i + 1] = buf;
-        }
-      } else {
-        if (new Intl.Collator(locale, {caseFirst: 'lower'}).compare(sorted[i], sorted[i + 1]) < 0) {
-          let buf = sorted[i];
-          sorted[i] = sorted[i + 1];
-          sorted[i + 1] = buf;
-        }
-      }
+  const compare = (a, b) => {
+    switch (param) {
+    case 'asc':
+      return collator.compare(a, b);
+    case 'desc':
+    default:
+      return collator.compare(b, a);
     }
-  }
+  };
 
-  return sorted;
+  return sorted.sort(compare);
 }
