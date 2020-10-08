eeec87698640748c28fe9f9f3eb8827181ade356
vladykod <Vladyko@htmacademy.ru>
2020-10-08 04:52:14 +0300

    решение задачи 2 (3 занятие)

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,18 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+//export function createGetter(path) {
+function createGetter(path) {
 
 }
+
+///
+const product = {
+  category: {
+    title: "Goods"
+  }
+};
+
+const getter = createGetter('category.title');
+
+console.log(getter(product));
