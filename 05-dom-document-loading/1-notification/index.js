c0fd53423cf0fd4aeaccd75b6dce6ab6435330e4
vladykod <Vladyko@htmacademy.ru>
2020-10-15 06:34:17 +0300

    решение задачи 5

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,39 @@
 export default class NotificationMessage {
 
+  constructor(
+    text = '',
+    obj = {}) {
+    this.text = text;
+    this.duration = obj.duration;
+    this.type = obj.type;
+  }
+
+  get template() {
+    return `
+      <div class="notification success" style="--value:${this.duration / 100}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.text}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  show() {
+    document.body.insertAdjacentHTML('beforeEnd', this.template);
+
+    let notificationArr = document.body.querySelectorAll('.notification');
+    let last = notificationArr[notificationArr.length - 1];
+
+    for (let i = 0; i < notificationArr.length - 1; i++) {
+      notificationArr[i].remove();
+    }
+
+    setTimeout(()=> {
+      last.remove();
+    }, this.duration * 10);
+  }
 }
