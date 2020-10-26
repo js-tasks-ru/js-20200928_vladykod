ce64c286ab119bde9f9b8838d19196430b312a77
vladykod <Vladyko@htmacademy.ru>
2020-10-26 03:07:59 +0300

    разбор домашки 6

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,83 @@
 class Tooltip {
+  static instance;
 
+  element;
+  vw = window.innerWidth;
+  vh = window.innerHeight;
+
+  onMouseOver = event => {
+    const element = event.target.closest('[data-tooltip]');
+
+    if (element) {
+      this.render(element.dataset.tooltip);
+      this.moveTooltip(event);
+
+      document.addEventListener('pointermove', this.onMouseMove);
+    }
+  };
+
+  onMouseMove = event => {
+    this.moveTooltip(event);
+  };
+
+  onMouseOut = () => {
+    this.removeTooltip();
+  };
+
+  removeTooltip() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+
+      document.removeEventListener('pointermove', this.onMouseMove);
+    }
+  }
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  initEventListeners() {
+    document.addEventListener('pointerover', this.onMouseOver);
+    document.addEventListener('pointerout', this.onMouseOut);
+  }
+
+  initialize() {
+    this.initEventListeners();
+  }
+
+  render(html) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = html;
+
+    document.body.append(this.element);
+  }
+
+  moveTooltip(event) {
+    let left = event.clientX + 10;
+    let top = event.clientY + 10;
+
+    if (this.element.offsetHeight + top + 10 > this.vh) {
+      top = this.vh - this.element.offsetHeight - 10;
+    }
+    if (this.element.offsetWidth + left + 10 > this.vw) {
+      left = this.vw - this.element.offsetWidth - 10;
+    }
+
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.onMouseOver);
+    document.removeEventListener('pointerout', this.onMouseOut);
+    this.removeTooltip();
+  }
 }
 
 const tooltip = new Tooltip();
